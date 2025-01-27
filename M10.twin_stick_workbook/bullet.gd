10f56fe9390802b888c17c380c3ac326a5e71960
ellaclz <jacaluza@s.sfusd.edu>
2025-01-27 14:05:40 -0800

    L6 L7 both

--- M10.twin_stick_workbook/bullet.gd
+++ M10.twin_stick_workbook/bullet.gd
@@ -1,15 +1,12 @@
 class_name Bullet extends Area2D
 
-@export var speed := 750
-
-var max_range := 1000.0
-
-var _traveled_distance = 0.0
+@export var speed := 100.0
+@export var max_range := 200.0
+@export var _traveled_distance = 0.0
 
 func _physics_process(delta: float) -> void:
 	var distance := speed * delta
 	var motion := Vector2.RIGHT.rotated(rotation) * distance
-
 	position += motion
 
 	_traveled_distance += distance
