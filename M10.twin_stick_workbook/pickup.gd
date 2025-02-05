2dda7fb08426cc4ed6fb4668f72190ea42276338
ellaclz <jacaluza@s.sfusd.edu>
2025-02-05 13:44:00 -0800

    L11 Not Finished
    Fix Mob & PickUp & Health Bar

--- M10.twin_stick_workbook/pickup.gd
+++ M10.twin_stick_workbook/pickup.gd
@@ -5,22 +5,28 @@ class_name Pickup extends Area2D
 
 @onready var _sprite_2d: Sprite2D = %Sprite2D
 @onready var _audio_stream_player: AudioStreamPlayer2D = %AudioStreamPlayer2D
-@onready var _animation_player: AnimationPlayer = %AnimationPlayer
+
 
 func _ready() -> void:
-	set_item(item)
+	body_entered.connect(_pickup)
+	area_entered.connect(_on_area_entered)
+	play_floating_animation()
 	
-	_animation_player.plau("idle")
-	body_entered.connect(func (body: Node2D) -> void:
-		if body is Player:
-			item.use(body)
-		_animation_player.play("destroy")
-		set_deferred("monitoring", false)
-		_audio_stream_player.play()
-		_animation_player.animation_finished.connect(func (_animation_name: String) -> void:
-			queue_free()
-		)
-	)
+func _pickup (body: Node2D) -> void:
+	if body is Player: 
+		item.use(body)
+		queue_free()
+
+func play_floating_animation() -> void:
+	var tween := create_tween()
+	tween.set_trans(Tween.TRANS_SINE)
+	
+	var sprite_2d := get_node("Sprite2D")
+	var position_offset := Vector2(0.0, 4.0)
+	var duration = randf_range(0.8, 1.2)
+	tween.tween_property(sprite_2d, "position", position_offset, duration)
+	tween.tween_property(sprite_2d,"position", -1.0 * position_offset, duration)
+	tween.set_loops()
 
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings := PackedStringArray()
@@ -30,4 +36,12 @@ func _get_configuration_warnings() -> PackedStringArray:
 	
 
 func set_item(value: Item) -> void:
-	item 
+	item = value 
+	if _sprite_2d != null:
+		_sprite_2d.texture = item.texture
+	if _audio_stream_player != null: 
+		_audio_stream_player.stream = item.sound_on_pickup
+		
+
+func _on_area_entered(area: Area2D) -> void:
+	queue_free()
