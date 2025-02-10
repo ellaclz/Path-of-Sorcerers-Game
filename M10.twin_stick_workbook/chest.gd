2566fcc6110dbde52d556f1f98b230bd5729c98f
ellaclz <jacaluza@s.sfusd.edu>
2025-02-10 15:47:58 -0800

    Lesson 12
    fix chest

--- M10.twin_stick_workbook/chest.gd
+++ M10.twin_stick_workbook/chest.gd
@@ -5,14 +5,13 @@ class_name Chest extends Area2D
 
 var is_player_near := false
 
+@onready var canvas_group: CanvasGroup = $%CanvasGroup
 @onready var _animation_player := $AnimationPlayer as AnimationPlayer
 
 func _ready() -> void:
-	@warning_ignore("unused_parameter")
 	body_entered.connect(func (body: Node) -> void:
 		is_player_near = true
 	)
-	@warning_ignore("unused_parameter")
 	body_exited.connect(func (body: Node) -> void:
 		is_player_near = false
 	)
@@ -33,9 +32,9 @@ func _unhandled_input(event: InputEvent) -> void:
 func create_pickup() -> void:
 	if possible_items == []:
 		return
-		
+
 	var item: Item = possible_items.pick_random()
-	var pickup: Pickup= preload("res://pickup.tscn").instantiate()
+	var pickup: Pickup = preload("res://pickup.tscn").instantiate()
 	pickup.item = item
 	add_child(pickup)
 	
@@ -43,7 +42,7 @@ func create_pickup() -> void:
 	var random_direction := Vector2(1.0, 0.0).rotated(random_angle)
 	var land_position := random_direction * randf_range(60.0, 120.0)
 	
-	const FLIGHT_TIME := 0.4
+	const FLIGHT_TIME := 0.3
 	const HALF_FLIGHT_TIME := FLIGHT_TIME / 2.0
 	
 	var tween := create_tween()
@@ -51,11 +50,11 @@ func create_pickup() -> void:
 	pickup.scale = Vector2(0.25, 0.25)
 	tween.tween_property(pickup, "scale", Vector2(1.0, 1.0), HALF_FLIGHT_TIME)
 	tween.tween_property(pickup, "position:x", land_position.x, FLIGHT_TIME)
-	
 	tween = create_tween()
 	tween.set_trans(Tween.TRANS_QUAD)
 	tween.set_ease(Tween.EASE_OUT)
 	var jump_height := randf_range(30.0, 80.0)
-	tween.tween_property(pickup, "position:y", land_position.y - jump_height, HALF_FLIGHT_TIME)
+	tween.tween_property(pickup, "position:y", land_position.y -
+	jump_height, HALF_FLIGHT_TIME)
 	tween.set_ease(Tween.EASE_IN)
 	tween.tween_property(pickup, "position:y", land_position.y, HALF_FLIGHT_TIME)
